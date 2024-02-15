8cf30335d3aa38ab206d0f030e20af37c0fe03e2
spideypeter04@gmail.com <rero.reyes.up@phinmaed.com>
2024-02-15 17:01:58 +0800

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.stevenlopez.block2.p1.wundersale"
-    compileSdk = 34
+    compileSdk= 34
 
     defaultConfig {
         applicationId = "com.stevenlopez.block2.p1.wundersale"
@@ -22,18 +22,15 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
     }
 
     kotlinOptions {
@@ -50,22 +47,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // Loading button
+    // Custom dependencies
     implementation("br.com.simplepass:loading-button-android:2.3.0")
-
-    // Glide
     implementation("com.github.bumptech.glide:glide:4.13.0")
-
-    // Circular image
     implementation("de.hdodenhof:circleimageview:3.1.0")
-
-    // ViewPager2 indicator
     implementation("io.github.vejei.viewpagerindicator:viewpagerindicator:1.0.0-alpha.2")
-
-    // StepView
     implementation("com.shuhart.stepview:stepview:1.5.1")
-
-    // Android KTX
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
 
     // Dagger Hilt
@@ -73,86 +60,6 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.5.0")
 }
 
-// Required for Dagger Hilt
 kapt {
     correctErrorTypes = true
-}
-
-
-android {
-    namespace = "com.stevenlopez.block2.p1.wundersale"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.stevenlopez.block2.p1.wundersale"
-        minSdk = 34
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-}
-
-    android {
-        namespace = "com.stevenlopez.block2.p1.wundersale"
-        compileSdk = 34
-
-        defaultConfig {
-            applicationId = "com.stevenlopez.block2.p1.wundersale"
-            minSdk = 34
-            targetSdk = 34
-            versionCode = 1
-            versionName = "1.0"
-
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
-
-        buildTypes {
-            release {
-                isMinifyEnabled = false
-                proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-                )
-            }
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,15 @@
-
 buildscript {
     repositories {
         google()
     }
     dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        // Dagger Hilt Android Gradle plugin
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.5.0")
-
-        // Google Services plugin
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
         classpath("com.google.gms:google-services:4.4.1")
-
-        // Navigation Safe Args Gradle plugin
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     }
 }
-
-
 
 plugins {
     id("com.android.application") version "8.2.0" apply false
