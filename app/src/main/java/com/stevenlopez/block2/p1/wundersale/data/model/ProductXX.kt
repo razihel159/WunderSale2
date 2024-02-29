a72b92550ae44c0154b548f9944933e7f468e958
Mark Gelo Rioflorido <markgelorioflorido@gmail.com>
2024-03-01 00:53:44 +0800

    retrofit(not yet complete)

---
+++ app/src/main/java/com/stevenlopez/block2/p1/wundersale/data/model/ProductXX.kt
@@ -0,0 +1,15 @@
+package com.stevenlopez.block2.p1.wundersale.data.model
+
+data class ProductXX(
+    val brand: String,
+    val category: String,
+    val description: String,
+    val discountPercentage: Double,
+    val id: Int,
+    val images: List<String>,
+    val price: Int,
+    val rating: Double,
+    val stock: Int,
+    val thumbnail: String,
+    val title: String
+)
