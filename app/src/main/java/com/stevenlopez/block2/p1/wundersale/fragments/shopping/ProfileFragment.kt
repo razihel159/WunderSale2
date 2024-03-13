ce99d4c955c9daa23c4cde5df39f565c52867ed4
Mark Gelo Rioflorido <markgelorioflorido@gmail.com>
2024-03-14 03:24:26 +0800

    login retrofit now working/wrong password crash
    you can now log out

--- app/src/main/java/com/stevenlopez/block2/p1/wundersale/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/stevenlopez/block2/p1/wundersale/fragments/shopping/ProfileFragment.kt
@@ -1,7 +1,36 @@
 package com.stevenlopez.block2.p1.wundersale.fragments.shopping
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.stevenlopez.block2.p1.wundersale.Activity.LoginSignupActivity
 import com.stevenlopez.block2.p1.wundersale.R
 
-class ProfileFragment: Fragment(R.layout.fragment_profile) {
+class ProfileFragment: Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val logoutButton = view.findViewById<Button>(R.id.LogOut)
+
+        logoutButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Logged out", Toast.LENGTH_SHORT).show()
+            val intent = Intent(requireContext(), LoginSignupActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
 }
