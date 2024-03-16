2d20385bba4c05c136158bc2f1769572535f90b5
Mark Gelo Rioflorido <markgelorioflorido@gmail.com>
2024-03-16 14:58:07 +0800

    try

--- app/src/main/java/com/stevenlopez/block2/p1/wundersale/fragments/loginSignup/LoginFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.stevenlopez.block2.p1.wundersale.fragments.loginSignup
-
-import androidx.fragment.app.Fragment
-import com.stevenlopez.block2.p1.wundersale.R
-
-class LoginFragment: Fragment(R.layout.fragment_login) {
-}
--- app/src/main/java/com/stevenlopez/block2/p1/wundersale/fragments/loginSignup/SignupFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.stevenlopez.block2.p1.wundersale.fragments.loginSignup
-
-import androidx.fragment.app.Fragment
-import com.stevenlopez.block2.p1.wundersale.R
-
-class SignupFragment: Fragment(R.layout.fragment_signup) {
-}
